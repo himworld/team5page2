534de901f951f332bd2e72af1c758988b92dfd3d
himworld <148764207+himworld@users.noreply.github.com>
2023-11-13 10:08:56 +0900

    Update 3_년도선택.py

--- 3_년도선택.py
+++ 3_년도선택.py
@@ -17,3 +17,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# 사이드바에 버튼 생성
+if st.sidebar.button("다른 페이지로 이동"):
+    # 다른 Streamlit 웹 페이지의 URL
+    new_page_url = "https://team5page2.streamlit.app/"
+
+    # 페이지를 새 URL로 리다이렉트
+    st.experimental_rerun()
+
+# 이 페이지의 나머지 콘텐츠
+st.write("현재 페이지입니다.")
